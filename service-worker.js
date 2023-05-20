c334bf2d968075214d5d769ec6070413b45567b9
muktaking <yeakubinfire@gmail.com>
2023-05-20 20:57:23 +0600

    Some tweaks

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d12780eadc04313a5bb41ceed9bb600.js"
+  "/precache-manifest.4c0b144415e04fd5a65bcbd7782fa732.js"
 );
 
 self.addEventListener('message', (event) => {
